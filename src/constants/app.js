3d19e612d3401ab147c8cef0caa0824225fdd432
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-09-02 21:13:12 -0700

    update constants

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,6 +1,6 @@
 // App-Wide Constants
 export const APP_NAME = 'Lightning Decoder';
-export const APP_TAGLINE = 'Decode Lightning Network Requests';
+export const APP_TAGLINE = 'Decode Lightning Network Requests, A hosted mirror by Bitcoin HK';
 export const APP_SUBTAGLINE = 'BOLT11, LNURL, and Lightning Address'
 export const APP_INPUT_PLACEHOLDER = 'Enter Invoice';
-export const APP_GITHUB = 'https://github.com/andrerfneves/lightning-decoder';
+export const APP_GITHUB = 'https://github.com/bitkarrot/lightning-decoder';
